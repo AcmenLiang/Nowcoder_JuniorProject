d749464c77eab9c41c9f571f23e86bd45ede4a52
AcmenLiang <weifanghaoliang@163.com>
2018-06-28 13:18:00 +0800

    class_03 third push

--- class_03/PageDriven/manage.py
+++ class_03/PageDriven/manage.py
@@ -1,20 +1,56 @@
 # -*- encoding=UTF-8 -*-
 # 文件功能：外部的脚本控制文件，做一些数据库初始化，跑些任务等等；
 from nowstagram import app, db
+from nowstagram.models import User, Image, Comment
 from flask_script import Manager
-from nowstagram.models import User
+from sqlalchemy import or_, and_
+import random
 
-manager = Manager(app)
+manager = Manager(app)  # 声明脚本Manager的实例manager
 
 
+def get_image_url():
+    return 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'
+
+
+# ORM好处，将数据库的操作用面向对象的python语言表达出来，通过python语言来操作数据库的增删改查，而不必去底层实现；
 @manager.command
 def init_database():
     db.drop_all()
     db.create_all()
-    for i in range(0, 100):
-        db.session.add(User('牛客' + str(i), 'password' + str(i)))
-    db.session.commit()
+    # 100个用户，每个用户3张图片，每张图片3条评论
+    for user_i_id in range(0, 100):
+        db.session.add(User(u'牛客' + str(user_i_id + 1), 'password' + str(random.randint(0, 1000))))  # id从1开始
+        for image_j_id in range(0, 3):
+            # 看Image类中的构造函数__init__的参数是几个，这里就传入几个，相当于类的实例化(或者说是表中一条数据的建立)
+            # 第2个参数就是用的user_id，故从上面的遍历中直接传下来
+            db.session.add(Image(get_image_url(), user_i_id + 1))
+            for comment_k_id in range(0, 3):
+                db.session.add(Comment(u'aa' + str(comment_k_id), 1 + 3 * user_i_id + image_j_id, user_i_id + 1))
 
+    db.session.commit()
+'''
+    # 查询语句实例  User类是继承的SQLAlchemy的model类，model类中有query等一系列方法，所以这里可以之间用'.'来访问
+    # 这里打印出来的格式就是User类中__repr__函数的格式，比如User类中是return '[User %d %s]' % (self.id, self.username)
+    # 则就是打印出的[User 3 牛客3]， __repr__就是用于查询函数要查询的东西的内容及格式。
+    print 'test1', User.query.all()  # 表示查询users表的所有
+    print 'test2', User.query.get(3)  # 查询users表中主键为3的数据
+    print 'test3', User.query.filter_by(id=5).first()  # filter_by限制条件，即限制id=5；first表示id=5的第一条数据
+    print 'test4', User.query.order_by(User.id.desc()).offset(1).limit(2).all()
+    # order_by表示对数据集排序；desc表示降序排列；offset表示偏移1个，即从第2个开始；limit表示只要2个；all表示查到的所有的都打出来；
+    # 这句相当于SQL语句的：select * from users order by id desc limit 1,2  这就是ORM的好处，建立数据库与对象之间的一一映射。
+    print 'test5', User.query.filter(User.username.endswith('0')).limit(3).all()
+    # filter表示限制条件，此处条件（endswith）为User类中的表为username字段的结尾为'0'的数据；limit限制为3个；all表示所有的都打出来；
+    # 其实不加all的话就变成输出的是SQL语句的形式，即在MySQL等软件中也可以操作的语句；
+    print 'test6', User.query.filter(or_(User.id == 88, User.id == 99)).all()
+    # or_是sqlalchemy中的一个实例或类，用于限制条件的或链接，即filter限制id=88或id=99的数据；
+    print 'test7', User.query.filter(and_(User.id > 88, User.id < 93)).all()
+    # and_同理，就是与的意思，对限制条件进行与，同时符合；
+    print 'test8', User.query.filter(and_(User.id > 88, User.id < 93)).first_or_404()
+    # 查询条件同上，first_or_404表示返回查询的第一个结果,如果没有结果,则终止请求,返回 404 错误响应
+    print 'test9', User.query.order_by(User.id.desc()).paginate(page=1, per_page=10).items
+    # ordr_by是逆序一下，上面用过，随便组合；paginate表示对查询的数据分页，即此处的每页10条数据，找出第1页的来；items即将数据提出来，不然只是数据的位置而不是具体内容；
+'''
 
 if __name__ == '__main__':
     manager.run()
--- class_03/PageDriven/nowstagram/models.py
+++ class_03/PageDriven/nowstagram/models.py
@@ -1,22 +1,99 @@
 # -*- encoding=UTF-8 -*-
-# 文件功能：对应数据库中的各个表的类写在该文件中；比如User类就是数据库中的users表；
-
+'''
+文件功能：对应数据库中的各个表的类写在该文件中；比如User类就是数据库中的users表；所有类的基类叫做db.Model，
+它存储在您必须创建的 SQLAlchemy 实例上。
+'''
 
 from nowstagram import db
+from datetime import datetime
+import random
 
 
-# 所有类的基类叫做db.Model，它存储在您必须创建的 SQLAlchemy 实例上。
-
-
+# 对应数据库中的users表
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # primary_key=True表示将id设为主键，即id值是唯一标识；
-    # autoincrement=True表示该列的id生成的序号是自己产生，且是自增的
-    username = db.Column(db.String(80), unique=True)  # unique=True指这个字段在这列里不能重复
-    password = db.Column(db.String(80))
+    __tablename__ = 'users'  # model类下面的属性，更改表的名字
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # primary_key=True表示将id设为主键，即id值是唯一标识；autoincrement=True表示该列的id生成的序号是自己产生，且是自增的；
+    # 即id这个键的值不需要初始化了(__init__函数不需要再写id的初始化内容)，加上了autoincrement后就会从1开始自增，直至不
+    # 在session.add数据为止；
 
+    username = db.Column(db.String(80), unique=True)  # unique=True指这个字段在这列里不能重复
+    password = db.Column(db.String(32))  # Column代表数据库中的一列，即关键字
+    head_url = db.Column(db.String(256))
+    images=db.relationship('Image')
+    # 含义解释：将两个表(User与Image)关联起来了，表示User中的image是从Image类来的；
+    # relationship中的第一个参数是要关联的类(表)的名称，这位类名Image，而不是__tablename__的images；
+    # 表示一个人有很多图片，即users类中的主键id可以对应许多images，即一对多的关系，用relationgship关联；
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+
+    # __init__相当于构造函数，声明一个实例的时候会通过这里进行属性的初始化；self即相当于this指针
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.head_url = 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'  # 随机生成头像的一种方式
 
+    # 这里打印出来的格式就是User类中__repr__函数的格式，比如User类中是return '[User %d %s]' % (self.id, self.username)
+    # 则就是打印出的[User 3 牛客3]， __repr__就是用于查询User.query()函数要查询的东西的内容及格式。
     def __repr__(self):
         return '[User %d %s]' % (self.id, self.username)
+
+
+# 每个人都可能发表若干张图片，此类对应数据库中的images表
+class Image(db.Model):
+    __tablename__ = 'images'  # 表名
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 图片的ID，主键
+    url = db.Column(db.String(512))  # 图片的URL地址
+    created_date = db.Column(db.DateTime)  # 图片发表时间，用DateTime数据类型
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+    # 图片是那个用户ID发表的，则images表中的user_id是用users表中的id来的；他们有关联性，这是ORM的好处；
+    # users的id是主键，users是id的主键表；images相对于users中的id来说是外键表；
+    # 想让主键表中的键值直接过来用ForeignKey函数来实现；
+
+    comments=db.relationship('Comment')
+    # 含义解释：将两个表(Image与Comment)关联起来了，表示Image中的comments是从Comment类来的；
+    # 表示一个图片(当前的类/表)有很多评论(要关联的东西)，即Image类中的主键id可以对应许多comments，即一对多的关系，用relationgship关联；
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+
+    # 构造函数
+    def __init__(self, url, user_id):
+        self.url = url
+        self.created_date = datetime.now()
+        self.user_id = user_id
+
+    # 查询函数(Image.query())数据查询后的内容和格式就是__repr__函数定义的，比如这里查询出来的只有图片id 图片url；
+    def __repr__(self):
+        return '[Image %d %s]' % (self.id, self.url)
+
+
+# 每张图片下都可能有若干条评论；此类对应数据库中的comments表；
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 评论的ID，设为主键
+    content = db.Column(db.String(1024))  # 评论内容
+    status = db.Column(db.Integer, default=0)  # 评论的状态，0表示正常，1表示被删除；默认为0
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # 该评论是哪个人(users.id)评论的，跟users表中的主键id有关联，需要用到users.id的值，则用ForeignKey函数；类似于某个表
+    # 的某个键值需要用到其他表的键值时，就要用ForeignKey这样的函数；比如上面的images.id需要用到users.id一个道理；
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+
+    image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+    # 该评论是评给哪个图的，跟上面一个道理，需要关联images.id中的键值，要用ForeignKey函数进行取images.id中的值；
+    # 即comments.image_id跟images.id关联起来了；
+
+    users=db.relationship('User')
+    # 含义解释：将两个表(Comment与User)关联起来了，表示Comment中的users是从User类来的；
+    # 表示一个评论(当前的类/表)要跟User(要关联的东西)关联起来，用relationgship关联；
+    # ForeignKey仅仅是取其他表中键的值，而并非关联起来，是表1的id用了表2的id，则用ForeignKey拷过来；若是关联，则用relationship
+
+    # 构造函数
+    def __init__(self, content, image_id, user_id):
+        self.content = content
+        self.image_id = image_id
+        self.user_id = user_id
+
+    # 查询函数(Comment.query())数据查询后的内容和格式就是__repr__函数定义的，比如这里查询出来的只有评论id 和 评论内容；
+    def __repr__(self):
+        return '[Comment %d %s]' % (self.id, self.content)
